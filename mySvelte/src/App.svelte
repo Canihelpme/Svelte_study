eb4f3e63056212644ff7530936fbc7f4572320d7
Donghyun Shin <donghyunshin@Donghyuns-MacBook-Pro.local>
2023-04-16 00:45:54 +0900

    First Svelte study

---
+++ mySvelte/src/App.svelte
@@ -0,0 +1,93 @@
+<script>
+	import home from './home.svelte';
+	import Router from 'svelte-spa-router';
+	import Header from './Header.svelte';
+
+	let name = 'world';
+	let age = 85;
+	let fruits = ['Apple','Banana','Cherry','Orange']
+	let toggle = false
+	
+	function assign() {
+		name = 'Donghyun'
+		age = 36
+	}
+	
+	function deleteFruit() {
+		fruits = fruits.slice(1)
+	}
+	
+	if (toggle) {
+	} else {
+	}	
+	
+	let isRed = false
+	
+	function enter() {
+		name = 'enter'
+	}
+	function leave() {
+		name = 'leave'
+	}
+	
+	const routes = {
+    '/': home,
+  };
+</script>
+
+<button on:click={() => {toggle = !toggle}}>
+	Toggle
+</button>
+
+{#if toggle}
+	<h1>Hello {name}! </h1>
+	{:else}
+<div> No name! </div>
+{/if}
+
+<h2 class={age < 85 ? 'active' : ''}>
+	{age}
+</h2>
+
+<img src="" alt={name} />
+
+<input type="text" bind:value={name} />
+
+<button on:click={assign}> Assign </button> 
+
+<div class="box" 
+		 style="background-color: {isRed ? 'red' : 'orange'};" 
+		 on:click={() => { isRed = !isRed}}
+	on:mouseenter={enter}
+	on:mouseleave={leave}>
+	Box!</div>
+
+<ul>
+	{#each fruits as fruit}
+		<li>{fruit}</li>
+	{/each}
+</ul>
+<button on:click={deleteFruit}>
+	Eat it!
+</button>
+
+<main>
+	<Header />
+  <Router {routes} />
+</main>
+
+
+<style>
+	h1 {
+		color : red;
+	}
+	.active {
+		color: blue;
+	}
+
+	.box {
+		width: 300px;
+		height: 150px;
+		background-color: orange;
+}
+</style>
---
+++ mySvelte/src/Header.svelte
@@ -0,0 +1,14 @@
+<!--./src/Component/Header/Header.svelte-->
+<nav>
+    <a href="#/">Home</a>
+    <a href="#/Contact1">Contact1</a>
+    <!-- App컴포넌트에서 '/Contact2/:test' 여기의 :test
+            가 1234가 된다.-->
+    <a href="#/Contact2/1234">Contact2</a>
+  </nav>
+  
+  <style>
+    a {
+      text-decoration: none !important;
+    }
+  </style>
